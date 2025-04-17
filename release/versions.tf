74fa5a5d07cc867f7fa0951fa2e61679d1aaccfc
Mo Mohoboob <momohob@amazon.com>
2025-04-17 15:45:30 +0100

    duplicate block

--- release/versions.tf
+++ release/versions.tf
@@ -5,13 +5,10 @@ terraform {
       version = "~> 5.38.0"
     }
   }
-}
 
-# Use Amazon S3 for Terraform backend
-terraform {  
   backend "s3" {  
     key          = "terraform/statefile.tfstate"
     encrypt      = true
-    use_lockfile = true  #S3 native locking
+    use_lockfile = true
   }
 }
